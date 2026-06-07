018fd8a429b838cb28d8ef972473779fe5ac9428
lihonnn <lihontan2@gmail.com>
2026-06-07 15:07:38 +0800

    updated ce2_q2 /models/dept.ts AGAIN  with comments for submissions

--- ce2/ce2_q2/models/dept.ts
+++ ce2/ce2_q2/models/dept.ts
@@ -15,13 +15,13 @@ export async function count(): Promise<DeptCount[]> {
     try {
         const [rows] = await db.pool.query<mysql.RowDataPacket[]>(` 
             SELECT code, count(id) as count FROM work GROUP BY code
-        `);                             // ^ SQL implementation
+        `);                             // ^ SQL implementation, [rows] destructure the [rows, fields] to return only [rows]
         const result: DeptCount[] = []; // initialise empty array to be returned later
         for (const row of rows) {       // use cursor to push each object to result array
             result.push(new DeptCount(row.count as number, row.code as string));  // count from total Sid per Code, Code from Dept
         }
         return result; // return result array
-        
+
     } catch (error) {
         console.error('database query failed. ' + error);
         throw error;
